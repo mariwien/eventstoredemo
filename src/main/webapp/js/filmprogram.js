65ac36574f448f28113c3c69ae57c714fb7ce8c0
Mari <wien@iterate.no>
2014-02-26 15:41:44 +0100

    forsøkt å pynte litt på frontend

---
+++ src/main/webapp/js/filmprogram.js
@@ -0,0 +1,15 @@
+
+var kinoApp = angular.module('phonecatApp',  []);
+
+
+
+kinoApp.controller('filmprogram', function ($scope, $http) {
+  $http.get('rest/filmprogram').success(function(data){
+    $scope.filmprogram = data;
+  });
+
+  $scope.opprettFilm = function(){
+    console.log($scope.navn, $scope.antallSeter);
+    $http.put('rest/filmprogram', {"navn":$scope.navn,"ledigeSeter":$scope.antallSeter})
+  };
+});
